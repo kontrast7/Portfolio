2fde7ac92146941f531a5e61ed748d2859d3af87
kontrast7 <47153257+kontrast7@users.noreply.github.com>
2021-12-03 21:18:57 +0300

    new portfolio

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
+import React from "react";
 import "./App.css";
-import { Header } from "./components/Header/Header";
-import { Main } from "./components/Main/Main";
-import { MySkills } from "./components/MySkils/MySkills";
-import { MyWorks } from "./components/MyWorks/MyWorks";
+import { BrowserRouter as Router } from "react-router-dom";
+import About from "./components/about/About";
+import { Works } from "./components/Works/Works";
+import { Skills } from "./components/skills/Skills";
+import { Sidebar } from "./components/sidebar/Sidebar";
+import { CarouselImages } from "./components/carousel/CarouselImages";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <MySkills />
-      <MyWorks />
-    </div>
+    <Router>
+      <div className="App">
+        <div className="side">
+          <nav className="navbar side navbar-expand-lg navbar-light p-0">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              style={{ zIndex: "1" }}
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <Sidebar />
+            </div>
+          </nav>
+        </div>
+        <div className="main">
+          <div>
+            <CarouselImages />
+          </div>
+          <About />
+          <Skills />
+          <Works />
+        </div>
+      </div>
+    </Router>
   );
-}
-
-export default App;
+};
--- src/components/Header/HeaderItem/HeaderItem.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import s from "./HeaderItem.module.css";
-
-export const HeaderItem = (props) => {
-  return <div className={s.item}>{props.nameLink}</div>;
-};
