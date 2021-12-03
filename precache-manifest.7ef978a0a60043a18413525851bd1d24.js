6eab5705dba8ab1bbed3b103078c2e8801cc5814
kontrast7 <47153257+kontrast7@users.noreply.github.com>
2021-12-03 21:24:12 +0300

    Updates

---
+++ precache-manifest.7ef978a0a60043a18413525851bd1d24.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89c222bf2a8de9165edc5e20afc2b87",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ab10d7ae919ca7ffec5f",
+    "url": "/Portfolio/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "9e8718be0cb17cd02b14",
+    "url": "/Portfolio/static/css/main.4b0be077.chunk.css"
+  },
+  {
+    "revision": "ab10d7ae919ca7ffec5f",
+    "url": "/Portfolio/static/js/2.d264f72f.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/Portfolio/static/js/2.d264f72f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e8718be0cb17cd02b14",
+    "url": "/Portfolio/static/js/main.7cebf429.chunk.js"
+  },
+  {
+    "revision": "6a58a16603ebe426610b",
+    "url": "/Portfolio/static/js/runtime-main.d3ebe2ed.js"
+  },
+  {
+    "revision": "5ae8366cbf86dceaaffd6c166a45cc50",
+    "url": "/Portfolio/static/media/Hello.5ae8366c.jpg"
+  },
+  {
+    "revision": "167fad74e2292a680a75ddfd47a1deb8",
+    "url": "/Portfolio/static/media/Pencil.167fad74.jpg"
+  }
+]);
