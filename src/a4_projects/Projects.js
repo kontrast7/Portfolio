bd3cec004e37e40b270936671f1a8a9fe2be457c
kontrast7 <maks.mashko@gmail.com>
2022-02-21 12:06:50 +0300

    add Projects

--- src/a4_projects/Projects.js
+++ src/a4_projects/Projects.js
@@ -8,7 +8,7 @@ import imgThree from '../assets/images/gallery-photo-3.png'
 import imgFour from '../assets/images/gallery-photo-4.png'
 import imgFive from '../assets/images/gallery-photo-5.png'
 import imgSix from '../assets/images/gallery-photo-6.png'
-// import imgSeven from '../assets/images/gallery-photo-7.jpg'
+import imgSeven from '../assets/images/gallery-photo-7.jpg'
 import imgEight from '../assets/images/gallery-photo-8.png'
 
 
@@ -21,42 +21,42 @@ function Projects() {
                     <Project
                         title={"todolist"}
                         img={imgOne}
-                        description={"TS, Redux, RestAPI, Thunk,Material UI, Unit Tests"}
-                        link={""}/>
+                        description={"TS, Redux, RestAPI, Thunk, Material UI, Unit Tests"}
+                        link={"https://github.com/kontrast7/Todolist-ts"}/>
 
                     <Project
                         title={"counter"}
                         img={imgTwo}
                         description={"My first project with React/Redux/TS.I left It here as a memory."}
-                        link={""}/>
+                        link={"https://kontrast7.github.io/Counter/"}/>
 
                     <Project
                         title={"the social network"}
                         img={imgThree}
                         description={"TS, Redux, RestAPI, Thunk,ANT Design, Unit Tests"}
-                        link={""}/>
+                        link={"https://github.com/kontrast7/The-way-of-the-samurai"}/>
 
                     <Project
                         title={"Study Point"}
                         img={imgFour}
                         description={"It was a command work. App for learning some cards"}
-                        link={""}/>
+                        link={"https://makssmeinart.github.io/cards-project/#/login"}/>
 
                     <Project
-                        title={"Rainfo"}
+                        title={"GitHub Search"}
                         img={imgFive}
-                        description={"It's my first project with Next.JS"}
-                        link={""}/>
+                        description={"Applications for finding users on GitHub and displaying repositories"}
+                        link={"https://git-search-m.vercel.app/"}/>
 
                     <Project
-                        title={"future project"}
+                        title={"SPA"}
                         img={imgSix}
-                        description={"...in progress"}
-                        link={"link #6"}/>
+                        description={"My first project on the layout of a single-page website"}
+                        link={"https://kontrast7.github.io/StormNet-Project/"}/>
 
                     <Project
                         title={"future project"}
-                        // img={imgSeven}
+                        img={imgSeven}
                         description={"...in progress"}
                         link={"link #1"}/>
 
